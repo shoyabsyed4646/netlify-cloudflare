d29dcc852711e55d2d75ffd5ab61760a8eba4909
shoyabsyed4646 <sshoyab@wearebarbarian.com>
2025-09-29 22:07:58 +0530

    netlify to cloudflare setup code

---
+++ src/main.js
@@ -0,0 +1,9 @@
+// On-scroll fade-in
+document.addEventListener("scroll", () => {
+  document.querySelectorAll(".fade-in").forEach((el) => {
+    const rect = el.getBoundingClientRect();
+    if (rect.top < window.innerHeight - 100) {
+      el.classList.add("visible");
+    }
+  });
+});
